ce00ec534fc04df6d8954d784c2d3a3b06e6e98a
ash2code <ashoksoma95@gmail.com>
2023-12-28 07:55:51 +0000

    first commit

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user to enter the first number
+
+# Prompt the user to enter the second number
+num1=5
+num2=4
+
+# Perform addition
+sum=$((num1 + num2))
+
+# Display the result
+echo "The sum of $num1 and $num2 is: $sum"
